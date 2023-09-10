ca9d9a7a1c3be421484341937c4e3af6c2972b6b
Sean Guno <91289730+seanguno@users.noreply.github.com>
2023-09-10 13:11:10 -0500

    Update circular-linked-list.hpp

--- includes/circular-linked-list.hpp
+++ includes/circular-linked-list.hpp
@@ -28,6 +28,122 @@ private:
   Order node_order_ = Order::kASC;
 };
 
+template<typename T>
+CircularLinkedList<T>::CircularLinkedList(const CircularLinkedList<T>& source) 
+: head_(nullptr), tail_(nullptr), node_order_(source.node_order_) {
+  if (source.head_ == nullptr) return;
+  Node<T>* curr = source.head_;
+  Node<T>* to_add = new Node(source.head_->data);
+  head_ = to_add;
+  tail_ = to_add;
+  head_->next = tail_;
+  while (curr->next != source.head_) {
+    curr = curr->next;
+   this->InsertInOrder(curr->data);
+  }
+}
+template<typename T>
+CircularLinkedList<T>& CircularLinkedList<T>::operator=(const CircularLinkedList<T>& source) {
+  if (this == &source) return *this;
+  // if (source.head_ == nullptr) {
+  //   this->~CircularLinkedList();
+  //   return *this;
+  // }
+  this->~CircularLinkedList();
+  if (source.head_ == nullptr) return *this;
+  node_order_ = source.node_order_;
+  Node<T>* curr = source.head_;
+  Node<T>* to_add = new Node(source.head_->data);
+  head_ = to_add;
+  tail_ = to_add;
+  head_->next = tail_;
+  while (curr->next != source.head_) {
+    curr = curr->next;
+    this->InsertInOrder(curr->data);
+  }
+  return *this;
+}
+
+template <typename T>
+CircularLinkedList<T>::~CircularLinkedList() {
+  if (head_ == nullptr) return;
+  tail_->next = nullptr;
+  while (head_->next != nullptr) {
+    Node<T>* next = head_->next;
+    delete head_;
+    head_ = next;
+  }
+  delete head_;
+  head_ = nullptr;
+  tail_ = nullptr;
+}
+
+template <typename T>
+void CircularLinkedList<T>::InsertInOrder(const T& data) {
+  Node<T>* curr = head_;
+  Node<T>* to_add = new Node(data);
+  if (head_ == nullptr) {
+    head_ = to_add;
+    tail_ = to_add;
+    head_->next = tail_;
+    return;
+  }
+  if (node_order_ == Order::kASC) {
+    if (data <= head_->data) {
+      to_add->next = head_;
+      head_ = to_add;
+      tail_->next = head_;
+      return;
+    }
+    while (curr->next->data < data) {
+      if (curr == tail_) break;
+      curr = curr->next;
+    }
+    to_add->next = curr->next;
+    curr->next = to_add;
+    if (curr == tail_) tail_ = to_add;
+  }
+  if (node_order_ == Order::kDESC) {
+    if (data >= head_->data) {
+      to_add->next = head_;
+      head_ = to_add;
+      tail_->next = head_;
+      return;
+    }
+    while (curr->next->data > data) {
+      if (curr == tail_) break;
+      curr = curr->next;
+    }
+    to_add->next = curr->next;
+    curr->next = to_add;
+    if (curr == tail_) tail_ = to_add;
+  }
+}
+
+template <typename T>
+void CircularLinkedList<T>::Reverse() {
+  if (node_order_ == Order::kASC) {
+    node_order_ = Order::kDESC;
+  } else if (node_order_ == Order::kDESC) {
+    node_order_ = Order::kASC;
+  }
+  if (head_ == nullptr) return;
+  Node<T>* next = head_->next;
+  Node<T>* curr = head_;
+  Node<T>* prev = tail_;
+  curr->next = prev;
+  prev = curr;
+  curr = next;
+  while (curr != head_) {
+    next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+  }
+  head_ = prev;
+  tail_ = curr;
+}
+
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const CircularLinkedList<T>& cll) {
   Node<T>* iter = cll.head_;
@@ -38,6 +154,8 @@ std::ostream& operator<<(std::ostream& os, const CircularLinkedList<T>& cll) {
   }
   // non-empty list condition
   do {
+    int test = iter->data;
+    test++;
     os << iter->data << '\t';
     iter = iter->next;
   } while (iter != cll.head_);
